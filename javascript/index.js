5e91bc66bf0329c0427c2014dd2f7b532e3ccc69
alu0100763379 <alu0100@ull.edu.es>
2015-11-24 22:35:44 +0000

    Añadida la barra de navegación y mejorada la estructura del código

--- javascript/index.js
+++ javascript/index.js
@@ -1,11 +1,40 @@
-function playPause(btn, vid) {
-  var vid = document.getElementById(vid);
-  if (vid.paused) {
-    vid.play();
-    btn.innerHTML = "Pause";
-  }
-  else {
-    vid.pause();
-    btn.innerHTML = "Play";
-  }
+//Inicialización de Variables
+
+var vid, btn, navbar;
+
+function intialize(){
+	
+	//Referenciando las variables
+	
+	vid = document.getElementById("mi_video");
+	btn = document.getElementById("btnplaypause");
+	navbar = document.getElementById("navbar");
+	
+	//Añadiendo Listeners
+	
+	btn.addEventListener("click",playPause,false);
+	navbar.addEventListener("change",vidBar,false);
+	vid.addEventListener("timeupdate",actualiza_navbar,false);
+}
+
+window.onload = intialize; //Lo que hace esta línea es asegurar que el HTML está cargado completamente. Así nos ahorramos errores al llamar a los elementos del HTML.
+
+function playPause(){
+	if(vid.paused){
+		vid.play();
+		btn.innerHTML = "Pause";
+	} else {
+		vid.pause();
+		btn.innerHTML = "Play";
+	}
+}
+
+function vidBar(){
+	var navbarto = vid.duration * (navbar.value / 100);
+	vid.currentTime = navbarto;
+}
+
+function actualiza_navbar(){
+	var new_time = vid.currentTime * (100 / vid.duration);
+	navbar.value = new_time;
 }
