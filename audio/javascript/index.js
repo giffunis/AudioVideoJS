f28b84d014617ca8212f01116e4c456a519de784
Rebecca <rebe.mart.gal@gmai.com>
2015-11-28 13:06:40 +0000

    reproductor de audio funcionando

--- audio/javascript/index.js
+++ audio/javascript/index.js
@@ -1,9 +1,54 @@
-var x = document.querySelector(".clickhere")
-function init () {
-  var val = document.querySelector(".val");
-  var source = document.querySelector("audio");
+//Definición de Variables
+var audioElm, inicio, masvel, menosvel;
 
-  source.src = val.value;
+function initialize () {
+  //Asignacion de su valor a cada variable
+  audioElm = document.getElementById("audio1");
+  inicio = document.getElementById("iniciar");
+  pp = document.getElementById('playbutton');
+  masvel = document.getElementById("masvel");
+  menosvel = document.getElementById("menosvel");
+
+  //EventListeners
+  inicio.addEventListener("click",iniciar,false);
+  pp.addEventListener("click",playPausa,false);
+  masvel.addEventListener("click",aumentarVel,false);
+  menosvel.addEventListener("click",disminuirVel,false);
 }
 
-  x.addEventListener('click',init,false);
+window.onload = initialize; //Lo que hace esta línea es asegurar que el HTML está cargado completamente. Así nos ahorramos errores al llamar a los elementos del HTML.
+
+function iniciar () {
+  if (audioElm.paused == true) {
+    // Get file from text box and assign it to the source of the audio element
+    audioElm.src = document.getElementById('audioFile').value;
+    audioElm.play();
+  }
+}
+function playPausa() {
+  if (document.getElementById("audio1")) {
+    if (audioElm.paused) {
+      audioElm.play();
+      document.getElementById("playbutton").innerHTML = "Pause"; // Set button text == Pause
+    }
+      else {
+        audioElm.pause();
+        document.getElementById("playbutton").innerHTML = "play"; // Set button text == Play
+      }
+  }
+}
+
+// Aumenta la velocidad del audio
+function aumentarVel() {
+  audioElm.playbackRate += 1;
+}
+
+// Dsminuye la velocidd del audio
+function disminuirVel() {
+  if (audioElm.playbackRate <= 1) {
+    var temp = audioElm.playbackRate;
+    audioElm.playbackRate = (temp / 2);
+  } else {
+    audioElm.playbackRate -= 1;
+  }
+}
